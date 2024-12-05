4004842694d58aade4c251ccfea650f8ddf802d0
Filip-Politowski <fil.politowski@gmail.com>
2024-12-05 15:05:54 +0100

    first commit

---
+++ src/main/java/pl/filip_politowski/mandaty/MandatyApplication.java
@@ -0,0 +1,13 @@
+package pl.filip_politowski.mandaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandatyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MandatyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/filip_politowski/mandaty/MandatyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filip_politowski.mandaty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandatyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
